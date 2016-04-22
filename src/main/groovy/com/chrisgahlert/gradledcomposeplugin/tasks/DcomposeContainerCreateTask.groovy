ec81be8d67abc916e7681dc3b78edd15d958b4dc
extr3m0 <extr3m0@email.de>
2016-04-22 11:14:42 +0200

    Increased test compatibility with older Docker versions

--- src/main/groovy/com/chrisgahlert/gradledcomposeplugin/tasks/DcomposeContainerCreateTask.groovy
+++ src/main/groovy/com/chrisgahlert/gradledcomposeplugin/tasks/DcomposeContainerCreateTask.groovy
@@ -362,6 +362,9 @@ class DcomposeContainerCreateTask extends AbstractDcomposeTask {
                 result.resolvConfPath = null
                 result.hostnamePath = null
                 result.networkSettings = null
+                if(!result.hostConfig.networkMode) {
+                    result.hostConfig.networkMode = 'default'
+                }
 
                 result
             }
